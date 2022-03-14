09695c2b9eb5e4a1d99a3fa267fae7c666a42c5d
Arbel Nathan <arbel.nathan@ibm.com>
2022-03-14 22:17:55 +0200

    call center + kafka (#1)
    * kafka files
    
    * upload readme for kafka
    
    * Update README-Kafka.md
    
    * moved all to kafka file and re-install dep (maybe we need again later)
    
    * add gitignore and remove node_modules
    
    * rename
    
    * remove redundant line
    
    Co-authored-by: jordantangy <jordantangy@gmail.com>
    Co-authored-by: jordantangy <49556564+jordantangy@users.noreply.github.com>

---
+++ callCenter/kafkaProduce.js
@@ -0,0 +1,33 @@
+// https://www.cloudkarafka.com/ הפעלת קפקא במסגרת ספק זה
+
+const uuid = require("uuid");
+const Kafka = require("node-rdkafka");
+
+const kafkaConf = {
+  "group.id": "cloudkarafka",
+  "metadata.broker.list": "tricycle-01.srvs.cloudkafka.com:9094,tricycle-02.srvs.cloudkafka.com:9094,tricycle-03.srvs.cloudkafka.com:9094".split(","),
+  "socket.keepalive.enable": true,
+  "security.protocol": "SASL_SSL",
+  "sasl.mechanisms": "SCRAM-SHA-256",
+  "sasl.username": "bo4lxb97",
+  "sasl.password": "LxAJNWji_ew_dvGIVejulDFaBoMb-yM6",
+  "debug": "generic,broker,security"
+};
+
+const prefix = "bo4lxb97-";
+const topic = `${prefix}default`;
+const producer = new Kafka.Producer(kafkaConf);
+
+const genMessage = m => new Buffer.alloc(m.length,m);
+
+producer.on("ready", function(arg) {
+  console.log(`producer Ariel is ready.`);
+});
+producer.connect();
+
+module.exports.publish= function(msg)
+{   
+  m=JSON.stringify(msg);
+  producer.produce(topic, -1, genMessage(m), uuid.v4());  
+  //producer.disconnect();   
+}
