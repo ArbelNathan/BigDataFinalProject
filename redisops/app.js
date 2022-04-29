cd0ae9c1a5346b4dac76c466b62420e3c3ab9c4e
Yehonatan <yehonatantseva@gmail.com>
2022-04-29 10:05:33 +0300

    Redis and dashboard (#7)
    * redis conf
    
    * Update Sender.ejs
    
    * Config fix
    
    * Update Sender.ejs
    
    * conf/bug fix
    
    * final redis
    
    * Update app.js
    
    * try to revert call center changes
    
    Co-authored-by: jordantangy <49556564+jordantangy@users.noreply.github.com>
    Co-authored-by: arbelnat <arbel.nathan@ibm.com>

---
+++ redisops/app.js
@@ -0,0 +1,105 @@
+const express = require('express');
+const app = express();
+var server = require('http').createServer(app);
+const io = require("socket.io")(server)
+const port = 3000
+
+
+//------------ kafka------------
+const kafka = require('./kafkaProduce');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//------------
+
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+
+app.get('/', (req, res) => res.send("<a href='/send'>Send</a>"));
+app.get('/send', (req, res) => res.render('sender'));
+
+app.get('/dashboard', function routeHandler(req ,res) {
+    var var1,var2,var3,var4,var5,var6;
+    const fs2 = require('fs');
+    fs2.readFile('./BigDataFinalProject/redisops/data/Calls_Sections.json', 'utf-8', (err, data) => {
+            if (err) {
+                throw err;
+            }
+            save2=JSON.parse(data.toString()); 
+            var1=parseInt(save2.one);var2=parseInt(save2.two);var3=parseInt(save2.three);
+            var4=parseInt(save2.four);var5=parseInt(save2.five);var6=parseInt(save2.six);
+            const fs3 = require('fs');
+            fs3.readFile('./data/call_details.json', 'utf-8', (err, data2) => {
+                 if (err) {
+                    throw err;
+            }
+            data2=data2.replace(/[^A-Za-z0-9]/g, " ");
+            data2=data2.replace(/data/g, "");
+            data2=data2.replace(/(\d+)/g, '\:$1\,');
+            res.render('dashboard',{one:var1,two:var2,three:var3,four:var4,five:var5,six:var6,json:data2.toString()} );
+    
+            });
+    
+    });
+    
+    
+        
+    });
+    
+    app.get('/p', function routeHandler(req ,res) {
+        const fs2 = require('fs');
+        fs2.readFile('./data/dashboard_table.json', 'utf-8', (err, data) => {
+                if (err) {
+                    throw err;
+                }
+                save2=JSON.parse(data.toString());
+                sum=0;
+                sum_positive=0;
+                for(let i=0;i<save2.array.length;i++){
+                    if(save2.array[i]!=0){
+                        sum=sum+save2.array[i];
+                    }
+                    if(i%7==0){
+                        if(save2.array[i]!=0)
+                           sum_positive=sum_positive+save2.array[i];
+                    }
+                }
+                acur=  parseInt(sum_positive/sum*100);
+                res.render('prediction',{array:save2.array,acur:acur} );
+        
+                });
+        
+        });
+
+
+
+//------------ Socket.io ----------------
+io.on("connection", (socket) => {
+    console.log("new user connected");
+    socket.on("totalWaitingCalls", (msg) => { console.log(msg.totalWaiting) });
+    socket.on("callDetails", (msg) => { console.log(msg);kafka.publish(msg) });
+    socket.on("Outcsv", () => { 
+        mongocsv.publish()
+        bigml.publish()});
+});
+
+
+//------------------- kafka -----------
+/* Kafka Producer Configuration */
+
+//
+//const client1 = new kafka.KafkaClient({kafkaHost: "localhost:9092"});
+
+
+
+
+
+
+//------------------------------------
+
+
+server.listen(port, () => console.log(`Ariel app listening at http://localhost:${port}`));
+
+
---
+++ redisops/kafkaConsume.js
@@ -0,0 +1,78 @@
+// https://www.cloudkarafka.com/ הפעלת קפקא במסגרת ספק זה
+
+const uuid = require("uuid");
+const Kafka = require("node-rdkafka");
+const { MongoBulkWriteError, MongoClient } = require("mongodb");
+//11/7
+var server = require('http').createServer(Kafka.KafkaConsume);
+const io = require("socket.io")(server)
+
+const kafkaConf = {
+  "group.id": "cloudkarafka-example",
+  "metadata.broker.list": "dory-01.srvs.cloudkafka.com:9094,dory-02.srvs.cloudkafka.com:9094,dory-03.srvs.cloudkafka.com:9094".split(","),
+  "socket.keepalive.enable": true,
+  "security.protocol": "SASL_SSL",
+  "sasl.mechanisms": "SCRAM-SHA-256",
+  "sasl.username": "ixylgbeh",
+  "sasl.password": "N-a_kLcPk1vTSfMqyBEN8gVmPg4XFwid",
+  "debug": "generic,broker,security"
+};
+
+const prefix = "ixylgbeh-";
+const topic = `${prefix}new`;
+const producer = new Kafka.Producer(kafkaConf);
+
+const genMessage = m => new Buffer.alloc(m.length,m);
+//const prefix = process.env.CLOUDKARAFKA_USERNAME;
+
+const topics = [topic];
+const consumer = new Kafka.KafkaConsumer(kafkaConf, {
+  "auto.offset.reset": "beginning"
+});
+
+consumer.on("error", function(err) {
+  console.error(err);
+});
+consumer.on("ready", function(arg) {
+  console.log(`Consumer ${arg.name} ready`);
+  consumer.subscribe(topics);
+  consumer.consume();
+});
+
+var AsyncLock = require('async-lock');
+var lock = new AsyncLock();
+
+function operation(id,m) {  //lock code- for Sync
+  lock.acquire(id, function(done) {
+      console.log(id + " Running operation")
+      setTimeout(function() { 
+      const DbHelper = require ('../mongo/Mongo'); // import all file and name it DbHelper
+      check(m.value.toString());  //we send the message to mongo db
+
+      var redis = require('../redis/RedisForArielSender')
+      sendredis(m.value.toString());
+          done();
+      }, 3000)
+  }, function(err, ret) {
+  }, {});
+}
+
+
+consumer.on("data", function(m) {
+  operation('key1',m)
+
+ 
+});
+
+
+consumer.on("disconnected", function(arg) {
+  process.exit();
+});
+consumer.on('event.error', function(err) {
+  console.error(err);
+  process.exit(1);
+});
+// consumer.on('event.log', function(log) {
+//   console.log(log);
+// });
+consumer.connect();
